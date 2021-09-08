d10c827a60d524729c22ae5ac79eaabb63f3aae4
rahulmandalkarsen386 <rahulmandalkar@seniority.in>
2021-09-08 10:56:33 +0530

    Merge branch 'master' of https://github.com/mieszko4/react-native-zoom-us into dev-upstream-update

--- index.ts
+++ index.ts
@@ -1,38 +1,57 @@
-import { NativeModules } from 'react-native'
+import { NativeModule, Platform } from 'react-native'
 import invariant from 'invariant'
-
-const { RNZoomUs } = NativeModules
+import { RNZoomUs } from './native'
 
 if (!RNZoomUs) console.error('RNZoomUs native module is not linked.')
 
 const DEFAULT_USER_TYPE = 2
 
-export interface RNZoomUsInitializeParams {
+interface RNZoomUsInitializeCommonParams {
+  domain?: string;
+  iosAppGroupId?: string;
+  iosScreenShareExtensionId?: string;
+}
+export interface RNZoomUsInitializeParams extends RNZoomUsInitializeCommonParams {
   clientKey: string;
   clientSecret: string;
-  domain?: string
 }
+
+export interface RNZoomUsSDKInitParams extends RNZoomUsInitializeCommonParams {
+  jwtToken: string;
+  // we don't care for the rest, for now
+}
+
 async function initialize(
-  params: RNZoomUsInitializeParams,
+  params: RNZoomUsInitializeParams|RNZoomUsSDKInitParams,
   settings: {
+    enableCustomizedMeetingUI?: boolean,
     // ios only
     disableShowVideoPreviewWhenJoinMeeting?: boolean
   } = {
+    enableCustomizedMeetingUI: false,
     // more details inside: https://github.com/mieszko4/react-native-zoom-us/issues/28
     disableShowVideoPreviewWhenJoinMeeting: true,
   },
-) {
+): Promise<string> {
   invariant(typeof params === 'object',
     'ZoomUs.initialize expects object param. Consider to check migration docs. ' +
     'Check Link: https://github.com/mieszko4/react-native-zoom-us/blob/master/docs/UPGRADING.md',
   )
 
-  invariant(params.clientKey, 'ZoomUs.initialize requires clientKey')
-  invariant(params.clientSecret, 'ZoomUs.initialize requires clientSecret')
+  if ('jwtToken' in params) {
+    invariant(params.jwtToken, 'ZoomUs.initialize requires jwtToken')
+  } else {
+    invariant(params.clientKey, 'ZoomUs.initialize requires clientKey')
+    invariant(params.clientSecret, 'ZoomUs.initialize requires clientSecret')
+  }
 
   if (!params.domain) params.domain = 'zoom.us'
 
   return RNZoomUs.initialize(params, settings)
+}
+
+function isInitialized(): Promise<boolean> {
+  return RNZoomUs.isInitialized()
 }
 
 export interface RNZoomUsJoinMeetingParams {
@@ -40,15 +59,33 @@ export interface RNZoomUsJoinMeetingParams {
   meetingNumber: string | number
   password?: string
   participantID?: string
+  autoConnectAudio?: boolean
   noAudio?: boolean
   noVideo?: boolean
+
+  // android only fields:
+  noInvite?: boolean
+  noBottomToolbar?: boolean
+  noPhoneDialIn?: boolean
+  noPhoneDialOut?: boolean
+  noMeetingEndMessage?: boolean
+  noMeetingErrorMessage?: boolean
+  noShare?: boolean
+  noTitlebar?: boolean
+
+  noButtonLeave?: boolean
+  noButtonMore?: boolean
+  noButtonParticipants?: boolean
+  noButtonShare?: boolean
+  noTextMeetingId?: boolean
+  noTextPassword?: boolean
 
   // ios only fields:
   zoomAccessToken?: string
   webinarToken?: string
 }
 async function joinMeeting(params: RNZoomUsJoinMeetingParams) {
-  let { meetingNumber, noAudio = false, noVideo = false } = params
+  let { meetingNumber, noAudio = false, noVideo = false, autoConnectAudio = false } = params
   invariant(meetingNumber, 'ZoomUs.joinMeeting requires meetingNumber')
   if (typeof meetingNumber !== 'string') meetingNumber = meetingNumber.toString()
 
@@ -58,6 +95,7 @@ async function joinMeeting(params: RNZoomUsJoinMeetingParams) {
     meetingNumber,
     noAudio: !!noAudio, // required
     noVideo: !!noVideo, // required
+    autoConnectAudio,   // required
   })
 }
 
@@ -72,6 +110,17 @@ export interface RNZoomUsStartMeetingParams {
   userId: string
   userType?: number // looks like can be different for IOS and Android
   zoomAccessToken: string
+
+  // android only fields:
+  noInvite?: boolean
+  noShare?: boolean
+
+  noButtonLeave?: boolean
+  noButtonMore?: boolean
+  noButtonParticipants?: boolean
+  noButtonShare?: boolean
+  noTextMeetingId?: boolean
+  noTextPassword?: boolean
 }
 async function startMeeting(params: RNZoomUsStartMeetingParams) {
   let { userType = DEFAULT_USER_TYPE, meetingNumber } = params
@@ -90,11 +139,95 @@ async function onMeetingEnded() {
   return RNZoomUs.onMeetingEnded();
 }
 
+async function leaveMeeting() {
+  return RNZoomUs.leaveMeeting()
+}
+
+async function connectAudio() {
+  return RNZoomUs.connectAudio()
+}
+
+async function isMeetingConnected() {
+  return RNZoomUs.isMeetingConnected()
+}
+
+async function isMeetingHost() {
+  return RNZoomUs.isMeetingHost()
+}
+
+async function getInMeetingUserIdList() {
+  return RNZoomUs.getInMeetingUserIdList()
+}
+
+async function rotateMyVideo(rotation: number) {
+  if (Platform.OS === 'android') {
+    return RNZoomUs.rotateMyVideo(rotation)
+  } else {
+    throw new Error('Only support android')
+  }
+}
+
+async function muteMyVideo(muted: boolean) {
+  return RNZoomUs.muteMyVideo(muted)
+}
+
+async function muteMyAudio(muted: boolean) {
+  return RNZoomUs.muteMyAudio(muted)
+}
+
+async function muteAttendee(userId: string, muted: boolean) {
+  return RNZoomUs.muteAttendee(userId, muted)
+}
+
+async function muteAllAttendee(allowUnmuteSelf: boolean) {
+  return RNZoomUs.muteAllAttendee(allowUnmuteSelf)
+}
+
+async function startShareScreen() {
+  return RNZoomUs.startShareScreen()
+}
+
+async function stopShareScreen() {
+  return RNZoomUs.stopShareScreen()
+}
+
+async function switchCamera() {
+  return RNZoomUs.switchCamera()
+}
+
+async function raiseMyHand() {
+  return RNZoomUs.raiseMyHand()
+}
+
+async function lowerMyHand() {
+  return RNZoomUs.lowerMyHand()
+}
+
+export const ZoomEmitter = RNZoomUs as NativeModule;
+
+export { default as ZoomUsVideoView } from './video-view'
+
 export default {
   initialize,
   joinMeeting,
   joinMeetingWithPassword,
   startMeeting,
   onMeetingStarted,
-  onMeetingEnded
+  onMeetingEnded,
+  leaveMeeting,
+  connectAudio,
+  isInitialized,
+  isMeetingHost,
+  isMeetingConnected,
+  getInMeetingUserIdList,
+  rotateMyVideo,
+  muteMyVideo,
+  muteMyAudio,
+  muteAttendee,
+  muteAllAttendee,
+  startShareScreen,
+  stopShareScreen,
+  switchCamera,
+  raiseMyHand,
+  lowerMyHand,
 }
